59ac2e28af5ed77dcde713f46706d20bb2628e66
umar <umars.salah@gmail.com>
2021-09-15 16:57:55 +0300

    Edit REsult COmponent Relates #3

--- src/Components/Card/Result/index.jsx
+++ src/Components/Card/Result/index.jsx
@@ -2,25 +2,26 @@ import React from "react";
 
 import "./style.css";
 
-export const Result = ({res, inputRef}) => {
+export const Result = ({ res, inputRef }) => {
   if (!res[0]) {
     return <h3>Result will be shown here</h3>;
   } else {
     const { word, meanings, origin, phonetic, phonetics } = res[0];
+    const definitionsArr = meanings.map((m) => m.definitions).map((def) => def[0]);
+
     return (
       <fieldset className="result">
-        <legend>{word}</legend>
+        <legend>{word.toUpperCase()}</legend>
         <label>phonetic : {phonetic} </label>
         <label>origin : {origin}</label>
+        {definitionsArr.map((e,i)=><li key={i}>{e.definition}Example: {e.example} </li>)}
         <div className="pronounciation">
-          pronounciation :
-          {/* <audio className="audio" controls >
-            <source src={isPlaying} type="audio/mp3" />
-          </audio> */}
-          <audio className="audio" ref={inputRef} controls >
+          pronunciation :
+          <audio className="audio" ref={inputRef} controls>
             <source src={phonetics[0].audio} type="audio/mp3" />
           </audio>
         </div>
+
       </fieldset>
     );
   }
