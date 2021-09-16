8a2ad88e9bdbf6664d5776c6003920caabcec047
NourAdawi <nouradwi12@gmail.com>
2021-09-16 12:33:36 +0300

    change desgin

--- src/Components/Card/Form/index.jsx
+++ src/Components/Card/Form/index.jsx
@@ -4,11 +4,12 @@ import './style.css';
 
 export const Form = ({ handleSubmit, handleChange }) => (
   <form className="form" onSubmit={handleSubmit}>
+      <p className="logo">Online dictionary</p>
     <input
       className="input"
       type="text"
       name="name"
-      placeholder="Enter the word you want to search for"
+      placeholder="Enter the word you want to search for .."
       onChange={handleChange}
     />
     <input className="submit" type="submit" value="Search" />
